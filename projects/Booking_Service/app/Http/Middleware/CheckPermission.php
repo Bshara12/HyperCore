d748936511db92194bbfecf448dcda01a57c33d8
FerasDeveloper <ferashatem38@gmail.com>
2026-06-30 23:40:54 +0300

    remove permissions

--- projects/Booking_Service/app/Http/Middleware/CheckPermission.php
+++ projects/Booking_Service/app/Http/Middleware/CheckPermission.php
@@ -8,13 +8,13 @@ class CheckPermission
 {
     public function handle($request, Closure $next, $permission)
     {
-        $user = $request->attributes->get('auth_user');
+        // $user = $request->attributes->get('auth_user');
 
-        if (! in_array($permission, $user['permissions'])) {
-            return response()->json([
-                'message' => 'Forbidden',
-            ], 403);
-        }
+        // if (! in_array($permission, $user['permissions'])) {
+        //     return response()->json([
+        //         'message' => 'Forbidden',
+        //     ], 403);
+        // }
 
         return $next($request);
     }
