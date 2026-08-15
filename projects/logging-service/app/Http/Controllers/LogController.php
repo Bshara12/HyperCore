cce524b9c4e080d7fa590875f50f67ee2ee38663
FerasDeveloper <ferashatem38@gmail.com>
2026-08-15 12:10:54 +0300

    test2

--- projects/logging-service/app/Http/Controllers/LogController.php
+++ projects/logging-service/app/Http/Controllers/LogController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 use stdClass;
 class LogController extends Controller
 {
+  // dwdwdwd
     // public function index(Request $request)
     // {
     //     $query = DB::table('logs');
